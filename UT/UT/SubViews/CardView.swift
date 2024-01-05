9f8b53330b579bd2ba840b0a3cbfb4c18f35eb0c
Dean <deancs1997@gmail.com>
2024-01-05 13:52:37 -0500

    Added app icon and missing
    Assets

---
+++ UT/UT/SubViews/CardView.swift
@@ -0,0 +1,18 @@
+//
+//  CardView.swift
+//  UT
+//
+//  Created by Dean Chong San on 2024-01-05.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardView()
+}
