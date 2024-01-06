fd09696b9a992aa60740ad0c882f8f0354094c62
Dean <deancs1997@gmail.com>
2024-01-06 14:45:04 -0500

    added macOS icon

---
+++ UT/UT/Models/PlayerWithPrices.swift
@@ -0,0 +1,8 @@
+//
+//  PlayerWithPrices.swift
+//  UT
+//
+//  Created by Dean Chong San on 2024-01-05.
+//
+
+import Foundation
---
+++ UT/UT/SubViews/AllStatsView.swift
@@ -0,0 +1,18 @@
+//
+//  AllStatsView.swift
+//  UT
+//
+//  Created by Dean Chong San on 2024-01-05.
+//
+
+import SwiftUI
+
+struct AllStatsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AllStatsView()
+}
