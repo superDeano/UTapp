1cd1b15a985e2ccc28f7cae0018658f5897a0f1b
Dean Chong San <deano@Deans-MacBook-Pro.local>
2024-02-04 15:46:53 -0500

    improved medium cardview

--- UT/UT/SubViews/MediumCardView.swift
+++ UT/UT/SubViews/MediumCardView.swift
@@ -22,40 +22,50 @@ struct MediumCardView: View {
                     ProgressView()
                 }
                 
+                //MARK: Player Face
+                CachedAsyncImage(url: URL(string: "\(ContentService.getPlayerFacesUrl() + (player.altimage ?? player.pid)).png")) {
+                    image in image.resizable().aspectRatio(contentMode: .fit)
+                        .frame(width: 125)
+                        .offset(x: 10, y: -15)
+                } placeholder: {
+                    ProgressView()
+                }
                 //MARK: Card info
                 VStack {
-
-                        //MARK: Player Face
-                        CachedAsyncImage(url: URL(string: "\(ContentService.getPlayerFacesUrl() + (player.altimage ?? player.pid)).png")) {
-                            image in image.resizable().aspectRatio(contentMode: .fill).frame(width: 125)
-                        } placeholder: {
-                            ProgressView()
-                        }
-                            .position(x: 100, y: 100)
                         
                         //MARK: Player Rating and Position
+                    HStack{
                         VStack {
                             Text(player.rating).foregroundStyle(Color.init(hexStr: player.itemInfo?.color ?? "453A22")).bold().font(.system(size: 26))
-                            Text(player.position).foregroundStyle(Color.init(hexStr: player.itemInfo?.color ?? "453A22")).multilineTextAlignment(.leading).font(.system(size: 20, weight: .regular)).fontWidth(.condensed)
-                        }.frame(alignment: .center).position(x: 42, y: 13)
+                            Text(player.position).foregroundStyle(Color.init(hexStr: player.itemInfo?.color ?? "453A22")).multilineTextAlignment(.leading).font(.system(size: 17, weight: .regular)).fontWidth(.condensed)
+                        }.frame(alignment: .center)
+                        Spacer()
+                    }.padding(.leading, 25)
+                        .offset(y: -25)
+//                        .padding(.top, 10)
                     
                     //MARK: Player Name
                     HStack{
-                        
                         Text(player.cardname).foregroundStyle(Color.init(hexStr: player.itemInfo?.color ?? "453A22")).font(.system(size: 20)).fontWidth(.condensed)
-                            .multilineTextAlignment(.center)
-                        
-                    }.position(x: 175/2,y:55)
+                            .multilineTextAlignment(.center).frame(maxWidth: 140)
+                    }.offset(y: 45)
+                    
+                    
                     //MARK: Nation, League, Team
-                    HStack {
+                    HStack(alignment:.center) {
+                        Spacer()
+                        Image("Nations/\(player.nation)").resizable().aspectRatio(contentMode: .fit).frame(width: 24, height: 24)
+                        Spacer()
+                        Image("Leagues/\(player.league)").resizable().aspectRatio(contentMode: .fit).frame(width: 24, height: 24)
+                        Spacer()
+                        Image("Clubs/\(player.club)").resizable().aspectRatio(contentMode: .fit).frame(width: 24, height: 24)
+                        Spacer()
+                    }.frame(maxWidth: 100, maxHeight: 20)
+                        .offset(y: 35)
                         
-                        Image("Nations/\(player.nation)").resizable().aspectRatio(contentMode: .fit).frame(width: 24)
-                        Image("Leagues/\(player.league)").resizable().aspectRatio(contentMode: .fit).frame(width: 24)
-                        Image("Clubs/\(player.club)").resizable().aspectRatio(contentMode: .fit).frame(width: 24)
-                    }.frame(maxWidth: 24, maxHeight: 20).position(x:175/2, y: 13)
                 }
             }
-        }.frame(width: 155, height: 250, alignment: .center).padding(.horizontal, 5)
+        }.frame(width: 155, height: 250, alignment: .center).padding(.horizontal, 10)
     }
     
 }
